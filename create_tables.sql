d4755f220aaa553f1b76e045968296ecabf89499
Sam Sahbaei Razavi <sam.sahbaei.razavi@iths.se>
2025-09-19 15:54:36 +0200

    Initial commit: created tables and inserted sample data

---
+++ create_tables.sql
@@ -0,0 +1,43 @@
+﻿
+USE MoviesLab2;
+GO
+
+
+IF OBJECT_ID('dbo.Movie', 'U') IS NOT NULL DROP TABLE dbo.Movie;
+IF OBJECT_ID('dbo.Genre', 'U') IS NOT NULL DROP TABLE dbo.Genre;
+GO
+
+
+CREATE TABLE dbo.Genre (
+    GenreId INT IDENTITY(1,1) PRIMARY KEY,   -- auto-numbered ID
+    Name    NVARCHAR(50) NOT NULL UNIQUE     -- must exist, no duplicates
+);
+GO
+
+
+CREATE TABLE dbo.Movie (
+    MovieId        INT IDENTITY(1,1) PRIMARY KEY,  -- auto-numbered ID
+    Title          NVARCHAR(200) NOT NULL,         -- required
+    ReleaseYear    INT NULL,                       -- optional
+    LengthMinutes  INT NULL,                       -- optional
+    Rating         INT NULL,                       -- optional (1..10)
+    GenreId        INT NOT NULL,                   -- required link to Genre
+
+
+    CONSTRAINT FK_Movie_Genre
+        FOREIGN KEY (GenreId) REFERENCES dbo.Genre(GenreId)
+        ON DELETE NO ACTION   -- block delete if movies still point to that genre
+        ON UPDATE NO ACTION,
+
+
+    CONSTRAINT CK_Movie_Year
+        CHECK (ReleaseYear IS NULL OR (ReleaseYear BETWEEN 1900 AND 2100)),
+    CONSTRAINT CK_Movie_Rating
+        CHECK (Rating IS NULL OR (Rating BETWEEN 1 AND 10))
+);
+GO
+
+
+CREATE INDEX IX_Movie_Title   ON dbo.Movie(Title);
+CREATE INDEX IX_Movie_GenreId ON dbo.Movie(GenreId);
+GO
---
+++ insert_data.sql
@@ -0,0 +1,20 @@
+﻿
+
+USE MoviesLab2;
+GO
+
+-- Inserting Genres
+INSERT INTO dbo.Genre (Name) VALUES
+('Action'),
+('Comedy'),
+('Drama'),
+('Sci-Fi'),
+('Horror');
+
+-- We then insert Movies
+INSERT INTO dbo.Movie (Title, ReleaseYear, LengthMinutes, Rating, GenreId) VALUES
+('The Matrix', 1999, 136, 9, 4),     -- Sci-Fi
+('The Godfather', 1972, 175, 10, 3), -- Drama
+('Die Hard', 1988, 132, 8, 1),       -- Action
+('Toy Story', 1995, 81, 8, 2),       -- Comedy
+('The Conjuring', 2013, 112, 7, 5);  -- Horror
